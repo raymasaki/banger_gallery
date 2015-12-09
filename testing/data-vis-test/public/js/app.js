39f11c7743c85a9f7b41ac47549bf2ab09984f14
Ray Masaki <ray@raymasaki.com>
2015-12-09 17:40:39 -0500

    added image fade in after load

--- testing/data-vis-test/public/js/app.js
+++ testing/data-vis-test/public/js/app.js
@@ -1,5 +1,17 @@
 var app = angular.module('mixtapeApp', ['ui.router'])
-  .config(MainRouter);
+  .config(MainRouter)
+  .directive('imgFadeInOnload', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attr) {
+        // once the image is loaded add the class 'loaded'
+        element.bind("load", function () {
+          element.addClass('loaded');
+        });
+        element.attr('src', attr.imgFadeInOnload);
+      }
+    };
+  });
 
 function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
